707cb973bf342746d646b46c4650aeca6e5d167f
katherine95 <kathiekim95@gmail.com>
2017-03-09 11:29:05 +0300

    working github events api

--- console_app_api.py
+++ console_app_api.py
@@ -1,7 +1,7 @@
 import requests #imports the request module
 
-print "Hello, register to get subscribed to or github events channel "
-github_name = input('Enter your github name: ')
-response  = requests.get('https://api.github.com/events'.format(github_name))
+print "Hello, register to get subscribed to github events channel "
+github_name = raw_input('Enter your github name: ')
+response  = requests.get('https://api.github.com/users/tater/events'.format(github_name))
 
 print response.json()
